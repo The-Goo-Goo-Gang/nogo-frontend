466041fadee60ae909b9f0ec844428931f5f5a15
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-03-25 14:55:15 +0800

    pref: home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,41 +3,56 @@
     <h1 class="game-title">NoGo</h1>
     <div class="game-actions">
       <RouterLink to="/game">
-        <button class="game-action-btn">Start Game</button>
+        <button class="game-action-btn">单人游戏</button>
       </RouterLink>
-      <br>
-      <button class="game-action-btn">Config</button>
-      <br>
-      <button class="game-action-btn">Exit</button>
+      <button class="game-action-btn">设置</button>
+      <button class="game-action-btn" @click="exit">退出</button>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {}
-})
+<script lang="ts" setup>
+const exit = () => {
+  window.electronAPI.exit()
+}
 </script>
 
 <style lang="scss">
+$theme-color: #2080f0;
+$bezier: cubic-bezier(0.4, 0, 0.2, 1);
+
 .game-home {
   text-align: center;
+  background: rgba(255, 255, 255, 0.35);
+  padding: 32px;
+  border-radius: 8px;
+}
+
+.game-title {
+  margin-top: 0;
+  -webkit-background-clip: text;
+  background-clip: text;
+  background-image: linear-gradient(252deg, rgba($color: $theme-color, $alpha: 0.6) 0%, $theme-color 100%);
+  -webkit-text-fill-color: transparent;
 }
 
 .game-actions {
-  display: flex;
+  display: inline-flex;
+  flex-wrap: nowrap;
   flex-direction: column;
+  border-radius: 8px;
 }
 
 .game-action-btn {
+  border-radius: 8px;
+  padding: 8px;
   background: none;
   border: 0;
+  transition: color 0.3s $bezier, background .3s $bezier;
 
   &:hover {
-    font-weight: bold;
+    color: white;
+    background-color: $theme-color;
   }
 }
 </style>
