32aa418b6854d5a461e85ef8cf71cf6c0600b780
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-03-25 14:55:23 +0800

    pref: game view

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="game-container">
-    <div class="game-chessboard">
-      <GameChessboard :width="400" :height="400"></GameChessboard>
+  <div class="game-container grid">
+    <div class="game-left-container left">
+      <PlayerIndicator :player="{name: 'Player 1', type: 0}" :is-playing="true" />
+      <div class="game-chessboard">
+        <GameChessboard :width="400" :height="400"></GameChessboard>
+      </div>
+      <PlayerIndicator :player="{name: 'Player 2', type: 0}" />
+    </div>
+    <div class="game-right-container right">
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import PlayerIndicator from '@/components/PlayerIndicator.vue'
 import GameChessboard from '@/components/GameChessboard.vue'
 </script>
 
@@ -19,13 +26,43 @@ import GameChessboard from '@/components/GameChessboard.vue'
   display: inline-block;
 }
 
+.game-left-container {
+  padding: 16px;
+  justify-content: space-between;
+  display: inline-flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.game-right-container {
+  padding: 16px;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  flex: 1;
+}
+
+.grid {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: stretch;
+  justify-content: space-around;
+  padding: 0em;
+}
+
 .game-container {
+  height: 94%;
   width: 96%;
   max-width: 1280px;
+
   @media (min-width: 600px) {
+    height: 92%;
     width: 94%;
   }
+
   @media (min-width: 1024px) {
+    height: 90%;
     width: 92%;
   }
 }
