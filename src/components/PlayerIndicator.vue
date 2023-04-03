60a8926daaacd8cdbcdd75cd9b8494d4bdf82b51
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-04-03 23:05:29 +0800

    feat: add hourglass anim in indicator

--- src/components/PlayerIndicator.vue
+++ src/components/PlayerIndicator.vue
@@ -6,14 +6,14 @@
         <img :src="player.avatar" v-else class="icon-2x" />
       </div>
       <div class="player-indicator-class">
-        <div>
-          <span v-if="isPlaying">Playing</span>
-          <AccountIcon class="player-indicator-type-icon" v-if="player.type == PlayerType.LocalHumanPlayer" />
-          <NetworkIcon class="player-indicator-type-icon" v-else-if="player.type == PlayerType.OnlineHumanPlayer" />
-          <RobotIcon class="player-indicator-type-icon" v-else-if="player.type == PlayerType.BotPlayer" />
-          <span>{{ player.name }}</span>
-        </div>
+        <Vue3Lottie class="player-indicator-hourglass" :animation-data="HourglassAnimJSON" height="1.5em" width="1.5em"
+          v-if="isPlaying" />
+        <AccountIcon class="player-indicator-type-icon" v-if="player.type == PlayerType.LocalHumanPlayer" />
+        <NetworkIcon class="player-indicator-type-icon" v-else-if="player.type == PlayerType.OnlineHumanPlayer" />
+        <RobotIcon class="player-indicator-type-icon" v-else-if="player.type == PlayerType.BotPlayer" />
+        <span>{{ player.name }}</span>
       </div>
+      <div class="player-indicator-chess-icon" :role="player.chess_type == Chess.Black ? 'black' : 'white'"></div>
     </div>
   </div>
 </template>
@@ -25,7 +25,9 @@ import NetworkIcon from 'vue-material-design-icons/Network.vue'
 import RobotIcon from 'vue-material-design-icons/Robot.vue'
 import { Player } from '@/state'
 import { defineProps } from 'vue'
-import { PlayerType } from '@/const'
+import { Chess, PlayerType } from '@/const'
+import HourglassAnimJSON from '@/assets/Hourglass_2.json'
+import { Vue3Lottie } from 'vue3-lottie'
 
 const props = withDefaults(defineProps<{
   player: Player,
@@ -61,7 +63,38 @@ const props = withDefaults(defineProps<{
   align-content: center;
 }
 
+.player-indicator-class {
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  gap: 8px;
+}
+
 .player-indicator-type-icon {
-  opacity: 0.7;
+  opacity: 0.75;
+}
+
+.player-indicator-hourglass {
+  position: relative;
+  bottom: -0.125em;
+  opacity: 0.5;
+}
+
+.player-indicator-chess-icon {
+  height: 1em;
+  width: 1em;
+  border-radius: 50%;
+  position: relative;
+  bottom: -0.125em;
+
+  &[role="black"] {
+    background-color: black;
+  }
+
+  &[role="white"] {
+    background-color: white;
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
 // import network from './network'
+import Vue3Lottie from 'vue3-lottie'
+import 'vue3-lottie/dist/style.css'
 
 createApp(App)
   // .use(network)
   .use(store, key)
   .use(router)
+  .use(Vue3Lottie, { name: 'Vue3Lottie' })
   .mount('#app')
